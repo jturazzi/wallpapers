9bb6438a5b2ed806fd0abcebee53e5da9aaf4ddf
Jérémy Turazzi <git@jtrz.fr>
2023-05-11 11:57:56 +0200

    Update galery

--- generateGallery.js
+++ generateGallery.js
@@ -4,18 +4,18 @@ const generateGallery = (imagesDir) => {
   // Crée le contenu HTML de la galerie
   let html = 
   `<!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="UTF-8">
-        <title>Galerie</title>
-        <link rel="stylesheet" href="https://bootswatch.com/5/vapor/bootstrap.min.css">
-      </head>
-      <body>
-        <div class="container">
-          <br>
-          <h1>Galerie</h1>
-          <br>
-          <div class="row row-cols-1 row-cols-md-3 g-4">
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Galerie</title>
+    <link rel="stylesheet" href="https://bootswatch.com/5/vapor/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container">
+      <br>
+      <h1>Galerie</h1>
+      <br>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
   `;
 
   // Parcourt le dossier d'images et ajoute une carte Bootstrap pour chaque image
@@ -23,26 +23,28 @@ const generateGallery = (imagesDir) => {
   for (const file of files) {
     if (file.match(/\.(png|jpg)$/)) {
       const name = file.replace(/\.[^/.]+$/, "");
-      html += `
+      html += 
+      `       
         <div class="col">
           <div class="card">
-            <a href="images/${file}">
-              <img src="images/${file}" class="card-img-top" alt="${name}">
+            <a href="${imagesDir}/${file}">
+              <img src="${imagesDir}/${file}" class="card-img-top" alt="${name}">
             </a>
             <div class="card-body">
               <h5 class="card-title">${name}</h5>
             </div>
           </div>
-        </div>`;
+        </div>
+        `;
     }
   }
 
   // Ajoute les scripts Bootstrap
-  html += `
-          </div>
-        </div>
-      </body>
-    </html>`;
+  html +=`   
+      </div>  
+    </div>
+  </body>
+</html>`;
 
   // Écrit le contenu HTML dans le fichier presentation.html
   fs.writeFileSync('index.html', html);
